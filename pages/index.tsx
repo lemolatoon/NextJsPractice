a49ffe0a3d71d7851d8840d7be4b07f0d94d2304
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-03-20 02:59:07 +0900

    Add second.tsx, canvas.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 import { NextPage } from "next";
-
-let i = 0;
+import Link from "next/link";
+import React, { useState } from "react";
+import { hello } from "../src/canvas";
 
 const Index: NextPage = () => {
+  const [count, setCount] = useState<number>(0);
+  const [msg, setMsg] = useState<string>("No Message");
 
-    return <button onClick={() => alert("clicked")}>Hello Next.js</button>
-}
+  function buttonOnChanged(event: React.ChangeEvent<HTMLInputElement>) {
+    setMsg(event.target.value);
+  }
 
-function inc() {
-    i++;
-}
-
-
-
+  return (
+    <>
+      <h1>Hello Next.js</h1>
+      <h2>{count}</h2>
+      <h2>
+        Here is input message you typed:<br></br>
+        {msg}
+      </h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          hello();
+        }}
+      >
+        count
+      </button>
+      <br></br>
+      <form>
+        <input type="text" onChange={buttonOnChanged}></input>
+      </form>
+      <br></br>
+      <Link href="./second">second</Link>
+    </>
+  );
+};
 
 export default Index;
---
+++ pages/second.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <h1>Hello from second page</h1>
+    </>
+  );
+};
+
+export default Index;
